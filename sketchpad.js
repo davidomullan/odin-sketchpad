945174c8d1512c5723a2e4a0c9148f6a94f86957
David O'Mullan <35239651+davidomullan@users.noreply.github.com>
2023-09-01 16:47:44 -0500

    Add working sketchpad and basic layout

---
+++ sketchpad.js
@@ -0,0 +1,33 @@
+function clearScreen(){
+	const screen = document.querySelector('.screen');
+	for (let i=0; i<screen.children.length; i++){
+		screen.children[i].style.backgroundColor = "white";
+	}
+}
+
+// Delete Screen Pixels
+function deletePixels(screen){
+	while (screen.firstChild){
+		screen.removeChild(screen.lastChild);
+	}
+}
+
+// Build screen
+function updateScreen(){
+	const screen = document.querySelector('.screen');
+	const screenSize = document.querySelector('#sketchpad-size').value;
+
+	deletePixels(screen);
+
+	for (let i=0; i<screenSize*screenSize; i++){
+		let newPixel = document.createElement('div');
+		newPixel.id = 'pixel' + i;
+		newPixel.className = 'pixel';
+		newPixel.style.height = `${100/screenSize}%`;
+		newPixel.style.width = `${100/screenSize}%`;
+		newPixel.addEventListener("mouseover", (event) => {
+			newPixel.style.backgroundColor = "black";
+		});
+		screen.appendChild(newPixel);
+	}
+}
